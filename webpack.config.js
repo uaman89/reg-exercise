56c621075668fe01b33fa0b9cc392379b32af923
uaman@gmail.com <git100500>
2017-10-14 15:15:05 +0300

    fix(buid): add missed config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                // use: [{
+                //     loader: 'babel-loader',
+                //     options: { presets: ['es2015'] },
+                // }],
+            },
+
+            // Loaders for other file types can go here
+        ]
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': { 'NODE_ENV': "'production'" }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ],
+};
