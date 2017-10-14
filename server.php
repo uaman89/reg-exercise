0194385266fe6d6c2086aa23ee3e91e4cf33ec71
uaman@gmail.com <git100500>
2017-10-14 21:29:35 +0300

    feat: update php script

--- server.php
+++ server.php
@@ -1,7 +1,19 @@
 <?php
+$fields = ['email', 'birthday', 'password1', 'password2'];
 
+var_dump($_POST);
 if (!empty($_POST)){
-    echo "<h1>Welcome, {$_POST['name']}!!!</h1>";
+
+    $user = $_POST['email'] ? $_POST['email'] : 'unknown user';
+
+    echo "<h1>Welcome, {$user}!!!</h1>";
+
+    foreach ($fields as $key){
+        if (!isset($_POST[$key])){
+            echo "<b>{$key}</b> - missed!<br/>";
+        }
+    }
+
 } else {
     echo "hmmm...";
 }
