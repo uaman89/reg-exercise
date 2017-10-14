24d468ba41cf067e35ddebccbc693fe059c62a4c
uaman@gmail.com <git100500>
2017-10-14 22:13:07 +0300

    feat: show preloader when submit

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,26 @@
 import {validateBirthday, validatePassword} from "./validation";
 import {post} from "./ajax"
 
-function submitForm(e){
-    console.log('e:', e);
+function submitForm(e) {
     e.preventDefault();
+
     document.querySelector("#submitBtn").disabled = "disabled";
+    var spinner = document.querySelector('#preloader');
+    spinner.style.display = 'inline-block';
 
     post('/server.php', new FormData(e.target)).then(
-        function(res){
-            console.log('res:', res);
+        function (res) {
             document.querySelector("#content").innerHTML = res;
-            window.history.pushState({},'/success');
+            spinner.style.display = 'none';
         },
-        function(err) {
+        function (err) {
             console.error('on post():', err);
-            alert("Sorry, can't sent data.")
+            alert("Sorry, can't sent data.");
+            spinner.style.display = 'none';
         }
     );
     return false;
 }
-
 
 window.onload = function () {
     document.querySelector("#birthday").onchange = validateBirthday;
